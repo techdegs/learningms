dffd06879a19ad660ef97338488946a8a60037a5
Samuel Osei Adu <asamuel355@yahoo.com>
2023-11-14 11:02:53 +0000

    updating-course

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -3,6 +3,7 @@ import { CatchAsyncErrors } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
+import CourseModel from "../models/course.model";
 
 export const uploadCourse = CatchAsyncErrors(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -18,11 +19,58 @@ export const uploadCourse = CatchAsyncErrors(
           url: myCloud.secure_url,
         };
       }
-    //   else{
-    //     return next(new ErrorHandler('Upload course thumbnail', 400));
-    //   }
+      //   else{
+      //     return next(new ErrorHandler('Upload course thumbnail', 400));
+      //   }
       createCourse(data, res, next);
       //res.json(data)
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+//Edit Course
+export const editCourse = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+
+      const courseId = req.params.id;
+
+      const course = await CourseModel.findByIdAndUpdate(
+        courseId,
+        { $set: data },
+        { new: true }
+      );
+
+      if (!courseId)
+        return next(
+          new ErrorHandler(
+            "Sorry something went wrong with this course. Try again later",
+            400
+          )
+        );
+
+      if (!course) return next(new ErrorHandler("Course not found", 400));
+
+      if (thumbnail) {
+        await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "Courses",
+        });
+
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+      res.status(201).json({
+        success: true,
+        course,
+        message: "Course updated successfully!.",
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 
 const courseRouter = express.Router();
 
@@ -9,6 +9,13 @@ courseRouter.post(
   isAuthenticated,
   authorizeRoles("admin"),
   uploadCourse
+);
+
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editCourse
 );
 
 export default courseRouter;

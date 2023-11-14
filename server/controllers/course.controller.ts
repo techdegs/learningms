2cf80404bb110e1de275066274e8bf05d3282586
Samuel Osei Adu <asamuel355@yahoo.com>
2023-11-14 11:58:49 +0000

    cache-courses-redis

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -4,6 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
+import { redis } from "../utils/redis";
 
 export const uploadCourse = CatchAsyncErrors(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -78,41 +79,95 @@ export const editCourse = CatchAsyncErrors(
 );
 
 //Get Single Course -- without purchasing - Everyone can access
-export const getSingleCourse = CatchAsyncErrors(async (req: Request, res: Response, next:NextFunction) => {
-  try {
-    const courseId = req.params.id;
-    if(!courseId) return next(new ErrorHandler('No Course Id. Try again', 400))
+export const getSingleCourse = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courseId = req.params.id;
+      if (!courseId)
+        return next(new ErrorHandler("No Course Id. Try again", 400));
 
-    const course = await CourseModel.findById(courseId).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+      const isCachedExist = await redis.get(courseId);
+      if (isCachedExist) {
+        console.log("Hitting redis db");
+        const course = JSON.parse(isCachedExist);
 
-    if(!course) return next(new ErrorHandler('Sorry Error occurred fetching the course', 400));
+        if (!course)
+          return next(
+            new ErrorHandler("Sorry Error occurred fetching the course", 400)
+          );
 
-    res.status(200).json({
-      success:true,
-      course,
-      message: 'Fetched Course successfully'
-    })
-  } catch (error:any) {
-    return next(new ErrorHandler(error.message, 400));
+        res.status(200).json({
+          success: true,
+          course,
+          message: "Fetched Course successfully",
+        });
+      } else {
+        console.log("Hitting mongo db");
+        const course = await CourseModel.findById(courseId).select(
+          "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+        );
+
+        if (!course)
+          return next(
+            new ErrorHandler("Sorry Error occurred fetching the course", 400)
+          );
+
+        //cache course
+        await redis.set(courseId, JSON.stringify(course));
+
+        res.status(200).json({
+          success: true,
+          course,
+          message: "Fetched Course successfully",
+        });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
   }
-})
-
+);
 
 //Get all courses for the public
-export const getAllCourses = CatchAsyncErrors(async(req:Request, res:Response, next:NextFunction) => {
-  try {
-    const courses = await CourseModel.find().select(
-      "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
-    );
+export const getAllCourses = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const isCachedCourses = await redis.get("allCourses");
+      if (isCachedCourses) {
+        console.log("Hitting Redis Db");
+        const courses = JSON.parse(isCachedCourses);
 
-    if(!courses) return next(new ErrorHandler('Sorry Error occurred fetching courses' , 400));
+        if (!courses)
+          return next(
+            new ErrorHandler("Sorry Error occurred fetching courses", 400)
+          );
 
-    res.status(200).json({
-      success: true,
-      courses,
-      message: 'Courses Fetched Successfully'
-    })
-  } catch (error:any) {
-    return next(new ErrorHandler(error.message, 400))
+        res.status(200).json({
+          success: true,
+          courses,
+          message: "Courses Fetched Successfully",
+        });
+
+      } else {
+        console.log("Hitting mongo db");
+        const courses = await CourseModel.find().select(
+          "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+        );
+
+        if (!courses)
+          return next(
+            new ErrorHandler("Sorry Error occurred fetching courses", 400)
+          );
+
+        await redis.set("allCourses", JSON.stringify(courses));
+
+        res.status(200).json({
+          success: true,
+          courses,
+          message: "Courses Fetched Successfully",
+        });
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
   }
-})
+);

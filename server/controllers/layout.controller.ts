f5b176a95b34518d592ad408ddab4980474f9b70
Samuel Osei Adu <asamuel355@yahoo.com>
2023-11-17 12:01:44 +0000

    update

--- server/controllers/layout.controller.ts
+++ server/controllers/layout.controller.ts
@@ -34,7 +34,10 @@ export const createLayout = CatchAsyncErrors(
           title,
           subTitle,
         };
-        const createBanner = await LayoutModel.create({type: "Banner", banner});
+        const createBanner = await LayoutModel.create({
+          type: "Banner",
+          banner,
+        });
 
         if (createBanner) {
           return res.status(201).json({
@@ -81,6 +84,95 @@ export const createLayout = CatchAsyncErrors(
         return res.status(201).json({
           success: true,
           message: "Categories created successfully",
+        });
+      } else {
+        return next(new ErrorHandler("Sorry choose input", 400));
+      }
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+//Edit layout
+export const editLayout = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body;
+
+      if (type === "Banner") {
+        const bannerData:any = await LayoutModel.findOne({type: "Banner"})
+        const {image, title, subTitle} = req.body;
+      
+        if(bannerData){
+          await cloudinary.v2.uploader.destroy(bannerData.banner.image.public_id);
+        }
+
+        const myCloud = await cloudinary.v2.uploader.upload(image, {
+          folder: "Layout",
+        });
+
+        const banner = {
+          image: {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+          },
+          title,
+          subTitle,
+        };
+        const updateBanner = await LayoutModel.findByIdAndUpdate(bannerData._id,{
+          type: "Banner",
+          banner,
+        });
+
+        if (updateBanner) {
+          return res.status(201).json({
+            success: true,
+            updateBanner,
+            message: "Banner updated successfully",
+          });
+        } else {
+          return next(
+            new ErrorHandler("Error occurred uploading banner image", 400)
+          );
+        }
+      } else if (type === "FAQ") {
+        const { faq } = req.body;
+        const faqItem = await LayoutModel.findOne({type: "FAQ"})
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer,
+            };
+          })
+        );
+
+        await LayoutModel.findByIdAndUpdate(faqItem?._id, { type: "FAQ", faq: faqItems });
+
+        return res.status(201).json({
+          success: true,
+          message: "FAQ updated successfully",
+        });
+
+      } else if (type === "Categories") {
+        const { categories } = req.body;
+        const categoryItem = await LayoutModel.findOne({ type: "Categories" });
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title,
+            };
+          })
+        );
+        await LayoutModel.findByIdAndUpdate(categoryItem?._id, {
+          type: "Categories",
+          categories: categoriesItems,
+        });
+
+        return res.status(201).json({
+          success: true,
+          message: "Categories updated successfully",
         });
       } else {
         return next(new ErrorHandler("Sorry choose input", 400));
